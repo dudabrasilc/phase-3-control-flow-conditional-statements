7c16defcca875c49db8e4e55d5f583c4927e7f21
Maria Coitinho <brasildu@gmail.com>
2022-10-27 17:20:31 -0500

    commit tree 993ec50c1c3f6af0c9bd70f14c8c2c0d1da403b8

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,73 @@
 def admin_login(username, password)
-  # your code here
+  if ((username == "admin" || username == "ADMIN") && password == "12345")
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
+
+# admin_login("sudo", "12345")
+# admin_login("admin", "12345")
+# admin_login("ADMIN", "12345")
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature <= 65 && temperature >= 40
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
 end
+
+# puts hows_the_weather(33)
+# puts hows_the_weather(99)
+# puts hows_the_weather(75)
+
 
 def fizzbuzz(num)
-  # your code here
+  if ((num % 3 == 0) && (num % 5 == 0))
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0 
+    "Buzz"
+  else
+    num
+  end
 end
+
+# puts fizzbuzz(1)
+# puts fizzbuzz(2)
+# puts fizzbuzz(3)
+# puts fizzbuzz(4)
+# puts fizzbuzz(5)
+# puts fizzbuzz(15)
 
 def calculator(operation, num1, num2)
-  # your code here
+  if operation == "+"
+    num1 + num2
+  elsif operation == "-"
+    num1 - num2
+  elsif operation == "*"
+    num1 * num2
+  elsif operation == "/"
+    num1 / num2
+  else
+    puts "Invalid operation!"
+  end
 end
 
+puts calculator("+", 1, 1)
+# => 2
+puts calculator("-", 3, 1)
+# => 2
+puts calculator("*", 3, 2)
+# => 6
+puts calculator("/", 4, 2)
+# => 2
+puts calculator("nope", 4, 2)
+# "Invalid operation!"
+# => nil
---
+++ run.rb
@@ -0,0 +1,15 @@
+dog = "cuddly"
+
+owner = 
+if dog == "hungry"
+  owner = "Refilling food bowl."
+elsif dog == "thirsty"
+  owner = "Refilling water bowl."
+elsif dog == "playful"
+  owner = "Playing tug-of-war."
+elsif dog == "cuddly"
+  owner = "Snuggling."
+else
+  owner = "Reading newspaper."
+end
+
